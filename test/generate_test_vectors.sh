8d5dd6411c81e3ead484eb2effecbf531b497909
OrthogonalHawk <OrthogonalHawk@gmail.com>
2019-06-07 15:24:50 -0500

    Tweaks to better support frequency shift testing

--- test/generate_test_vectors.sh
+++ test/generate_test_vectors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd utilities
-mkdir tmp_notebooks
+mkdir -p tmp_notebooks
 
 # 000 - generate a ramp pattern to verify proper C/C++ file parsing
 export FREQ_SHIFT=0
@@ -12,5 +12,15 @@ export OUT_FILE_NAME=../vectors/test_000
 export RAMP_OUTPUT=1
 
 jupyter nbconvert --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_000.ipynb
+
+# 001 - positive frequency shift test vector, no resampling
+export FREQ_SHIFT=100000
+export INPUT_SAMPLE_RATE=1000000
+export OUTPUT_SAMPLE_RATE=1000000
+export NUM_OUTPUT_SAMPLES=10000
+export OUT_FILE_NAME=../vectors/test_001
+export RAMP_OUTPUT=0
+
+jupyter nbconvert --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_001.ipynb
 
 cd ../
