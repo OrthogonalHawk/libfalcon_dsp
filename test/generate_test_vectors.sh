798fecccae1854bf262bac1c36aab4bb7bf5f944
OrthogonalHawk <OrthogonalHawk@gmail.com>
2019-08-30 11:16:08 -0500

    Adding C++ Resampler Tests (#1)
    Only a couple so far; plan to expand these.

--- test/generate_test_vectors.sh
+++ test/generate_test_vectors.sh
@@ -13,6 +13,7 @@ export RAMP_OUTPUT=1
 
 jupyter nbconvert --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_000.ipynb
 
+
 # 001 - positive frequency shift test vector, no resampling
 export FREQ_SHIFT=100000
 export INPUT_SAMPLE_RATE=1000000
@@ -22,6 +23,7 @@ export OUT_FILE_NAME=../vectors/test_001
 export RAMP_OUTPUT=0
 
 jupyter nbconvert --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_001.ipynb
+
 
 # 002 - positive frequency shift test vector, no resampling, long file
 export FREQ_SHIFT=100000
@@ -33,6 +35,7 @@ export RAMP_OUTPUT=0
 
 jupyter nbconvert --ExecutePreprocessor.timeout=300 --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_002.ipynb
 
+
 # 003 - negative frequency shift test vector, no resampling
 export FREQ_SHIFT=-5000
 export INPUT_SAMPLE_RATE=1000000
@@ -42,5 +45,27 @@ export OUT_FILE_NAME=../vectors/test_003
 export RAMP_OUTPUT=0
 
 jupyter nbconvert --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_003.ipynb
+
+
+# 004 - no frequency shift; simple 2x decimation
+export FREQ_SHIFT=0
+export INPUT_SAMPLE_RATE=1000000
+export OUTPUT_SAMPLE_RATE=500000
+export NUM_OUTPUT_SAMPLES=10000
+export OUT_FILE_NAME=../vectors/test_004
+export RAMP_OUTPUT=0
+
+jupyter nbconvert --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_004.ipynb
+
+
+# 005 - no frequency shift; 1 MHz -> 600 kHz
+export FREQ_SHIFT=0
+export INPUT_SAMPLE_RATE=1000000
+export OUTPUT_SAMPLE_RATE=600000
+export NUM_OUTPUT_SAMPLES=10000
+export OUT_FILE_NAME=../vectors/test_005
+export RAMP_OUTPUT=0
+
+jupyter nbconvert --to notebook --execute generate_polyphase_test_vectors.ipynb --output tmp_notebooks/generate_polyphase_test_vectors_005.ipynb
 
 cd ../
